a88de54adcab70ca3b32ecca7c21db1d7fed0f5f
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-04 20:13:47 +0200

    ya

--- ejercicio3.py
+++ ejercicio3.py
@@ -16,13 +16,15 @@ import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
 #restricciones
-f1 = "(8-2*x)/4" # 2x1 + 4x2 <= 8 -- azul
-f2 = "(4+x*1)/4" # -x1 + 4x2 <= 4 --naranja
-f3 = 'x*1-2' # x1 - x2 <= 2 -- verde
+f1 = "(8-2*x)/4" # 2x1 + 4x2 <= 8 -- azul, para abajo
+f2 = "(4+x*1)/4" # -x1 + 4x2 <= 4 --naranja, para abajo
+f3 = 'x*1-2' # x1 - x2 <= 2 -- verde, para arriba
 #función a maximizar
 z1 = '(20-6*x)/3' # 6x1 + 3x2 = 20
-z2 = '(37-6*x)/3' # 6x1 + 3x2 = 37
-z3 = '(30-6*x)/3' # 6x1 + 3x2 = 30
+z2 = '(15-6*x)/3' # 6x1 + 3x2 = 15
+z3 = '(10-6*x)/3' # 6x1 + 3x2 = 10
 
 x = symbols('x')
 plot(f1, f2, f3, z1,z2, z3, (x, 0, 5))
+
+#Es solución única, donde el punto corresponde a la recta morada que más o menos es x1=2,x2=1
