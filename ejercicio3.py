bf00406c9060b2f325516156286423d60e1b0701
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-04 20:03:27 +0200

    ya

--- ejercicio3.py
+++ ejercicio3.py
@@ -12,3 +12,17 @@ S. a.:
 x1 - x2 <= 2
 x1; x2 >= 0
 '''
+import sympy as sym
+from sympy import symbols
+from sympy.plotting import plot
+#restricciones
+f1 = "(8-2*x)/4" # 2x1 + 4x2 <= 8
+f2 = "(4+x*1)/4" # -x1 + 4x2 <= 4
+f3 = 'x*1-2' # x1 - x2 <= 2
+#función a maximizar
+z = '(20-6*x1)/3' # 6x1 + 3x2 = 20
+z = '(37-6*x1)/3' # 6x1 + 3x2 = 37
+z = '(30-6*x1)/3' # 6x1 + 3x2 = 30
+
+x = symbols('x')
+plot(f1, f2, f3, z, (x, -15, 15))
