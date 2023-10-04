662c0624578b0767d90bec5c62cbe2898d9c5ff7
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-04 19:53:19 +0200

    enunciados

---
+++ ejercicio3.py
@@ -0,0 +1,14 @@
+'''
+Representar gráficamente el problema y decidir en cuál de los cuatro casos nos encontramos:
+• Solución única
+• Solución múltiple
+• Solución no acotada
+• Solución no factible
+
+Max Z = 6 x1 + 3 x2
+S. a.:
+2 x1 + 4 x2 <= 8
+- x1 + 4 x2 <= 4
+x1 - x2 <= 2
+x1; x2 >= 0
+'''
---
+++ ejericcio4.py
@@ -0,0 +1,34 @@
+'''
+La industria de juguetes “Galaxia” produce dos tipos de juguetes:
+• Space Ray
+• Zapper
+Los recursos están limitados a:
+• 1200 kg de plástico por semana
+• 40 horas de producción semanal
+Los requerimientos de Marketing son:
+• La producción total no puede exceder de 800 docenas.
+• El número de docenas de Space Rays no puede exceder al número de docenas de 
+Zappers por más de 450.
+Los requerimientos Tecnológicos son:
+• Space Rays requiere 2 kg de plástico y 3 minutos de producción por docena.
+• Zappers requiere 1 kg de plástico y 4 minutos de producción por docena.
+El plan de producción actual se establece mediante los siguientes criterios:
+• Fabricar la mayor cantidad del producto que deje más beneficios, el cual corresponde 
+a Space Ray (8 € de utilidad por docena).
+• Usar la menor cantidad de recursos para producir Zappers, porque estos dejan una 
+menor utilidad (5 € de utilidad por docena).
+El plan de producción actual es, por tanto:
+• Space Rays = 550 docenas
+• Zappers = 100 docenas
+Obteniéndose una utilidad Z de 4.900 € por semana.
+Las variables de decisión son:
+• x1 = Producción de Space Rays (en docenas por semana)
+• x2 = Producción de Zappers (en docenas por semana)
+Se pide:
+a) Emplear el método gráfico para visualizar las restricciones. Calcular Z, x1 y x2 para el 
+plan de producción actual. (3 puntos)
+b) ¿Se puede hacer mejor? ¿Cómo? (4 puntos)
+c) Calcular Z, x1 y x2 para el mejor plan de producción sin Zapper (1 punto)
+d) Calcular Z, x1 y x2 para el mejor plan de producción sin Space Ray (1 punto)
+
+'''
