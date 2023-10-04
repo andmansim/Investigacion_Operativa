637e67f70caf77ad011b763499bb3a443a575985
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-04 20:06:39 +0200

    a

--- ejercicio3.py
+++ ejercicio3.py
@@ -16,13 +16,13 @@ import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
 #restricciones
-f1 = "(8-2*x)/4" # 2x1 + 4x2 <= 8
-f2 = "(4+x*1)/4" # -x1 + 4x2 <= 4
-f3 = 'x*1-2' # x1 - x2 <= 2
+f1 = "(8-2*x)/4" # 2x1 + 4x2 <= 8 -- azul
+f2 = "(4+x*1)/4" # -x1 + 4x2 <= 4 --naranja
+f3 = 'x*1-2' # x1 - x2 <= 2 -- verde
 #función a maximizar
-z = '(20-6*x1)/3' # 6x1 + 3x2 = 20
-z = '(37-6*x1)/3' # 6x1 + 3x2 = 37
-z = '(30-6*x1)/3' # 6x1 + 3x2 = 30
+z1 = '(20-6*x)/3' # 6x1 + 3x2 = 20
+z2 = '(37-6*x)/3' # 6x1 + 3x2 = 37
+z3 = '(30-6*x)/3' # 6x1 + 3x2 = 30
 
 x = symbols('x')
-plot(f1, f2, f3, z, (x, -15, 15))
+plot(f1, f2, f3, z1,z2, z3, (x, 0, 5))
