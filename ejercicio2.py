460abba08cb0ca2cb9620f005c69d01097844f5f
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-04 19:38:28 +0200

    c

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,13 @@
 import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
-f1 = "10-x" 
-f2 = "1+x" 
-f3 = "4" 
+f1 = "10-x" # x1 + x2 <= 10
+f2 = "1+x" # x1 -x2 <= 1
+f3 = "4" # x2 <= 4
 Z1 = "(16-x)/2"
 Z2 = "(14-x)/2"
 Z3 = "(13-x)/2"
 x = symbols('x')
 plot(f1, f2, f3, Z1, Z2, Z3, (x, -15, 15))
+
+#Solución: x2 = 4, x1= 5, para mantener la diferencia de 1 entre x1 y x2
--- ejercicio2_1.py
+++ ejercicio2_1.py
@@ -1,11 +1,13 @@
 import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
-f1 = "10-x" 
-f2 = "1+x" 
-f3 = "4" 
+f1 = "10-x" #x2 + x1 <= 10
+f2 = "1+x" #x1 -x2 <= 1
+f3 = "4" #x2 <= 4
 Z1 = "16-x"
 Z2 = "14-x"
 Z3 = "13-x"
 x = symbols('x')
 plot(f1, f2, f3, Z1, Z2, Z3, (x, -1, 15))
+#El area optima es la parte de abajo de la naranja, la azul y la verde --> colorear en un pdf
+#Solución: x2 = 4, x1= 5, para mantener la diferencia de 1 entre x1 y x2
