7cacb593ac3d0fae506baadb824bcdfdeaa99719
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-03 19:57:59 +0100

    código de los cuadrantes

--- Simetrias/codigo.py
+++ Simetrias/codigo.py
@@ -23,13 +23,45 @@ x2 <= 0
 import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
+
+x = symbols('x')
+
+#1er cuadrante
 f1 = "8-x" # x1 + x2 <= 8
 f2 = "18-3*x" # 3x1 + x2 <= 18
 # Max Z = 2x1 + x2
 z1 = "20-2*x " 
 z2 = '30-2*x'
 z3 = '40-2*x'
+plot(f1, f2, z1, z2, z3,(x, 0, 15), show=True, legend=True) 
 
-x = symbols('x')
-plot(f1, f2, z1, z2, z3,(x, 0, 15), show=False, legend=True)
-plot(f1, f2, z1, z2, z3,(x, -15, 0), show=False, legend=True)
+#2do cuadrante
+f1 = "8-x" # x1 + x2 <= 8
+f2 = "18-3*x" # 3x1 + x2 <= 18
+# Max Z = 2x1 + x2
+z1 = "20+2*x "
+z2 = '30+2*x'
+z3 = '40+2*x'
+plot(f1, f2, z1, z2, z3,(x, -15, 0), show=True, legend=True) 
+
+#3er cuadrante
+f1 = "8+x" # x1 + x2 <= 8
+f2 = "18+3*x" # 3x1 + x2 <= 18
+# Max Z = 2x1 + x2
+z1 = "20+2*x "
+z2 = '30+2*x'
+z3 = '40+2*x'
+plot(f1, f2, z1, z2, z3,(x, -15, 0), show=True, legend=True) 
+
+#4to cuadrante
+f1 = "8+x" # x1 + x2 <= 8
+f2 = "18+3*x" # 3x1 + x2 <= 18
+# Max Z = 2x1 + x2
+z1 = "20-2*x "
+z2 = '30-2*x'
+z3 = '40-2*x'
+plot(f1, f2, z1, z2, z3,(x, 0, 15), show=True, legend=True)
+
+
+
+
