af1318c3c99f21efdbcbba18fc33bbba31e76344
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-04 12:19:42 +0100

    listo

--- Simetrias/codigo.py
+++ Simetrias/codigo.py
@@ -27,37 +27,40 @@ from sympy.plotting import plot
 x = symbols('x')
 
 #1er cuadrante
-f1 = "8-x" # x1 + x2 <= 8
-f2 = "18-3*x" # 3x1 + x2 <= 18
-# Max Z = 2x1 + x2
+#x1 >= 0, x2 >= 0 
+f1 = "8-x" 
+f2 = "18-3*x" 
+
 z1 = "20-2*x " 
 z2 = '30-2*x'
 z3 = '40-2*x'
 plot(f1, f2, z1, z2, z3,(x, 0, 15), show=True, legend=True) 
 
 #2do cuadrante
-f1 = "8-x" # x1 + x2 <= 8
-f2 = "18-3*x" # 3x1 + x2 <= 18
-# Max Z = 2x1 + x2
+#x1 <= 0, x2 >= 0 --> reescribimos la x1: -x1 >= 0
+f1 = "8+x" 
+f2 = "18+3*x" 
+
 z1 = "20+2*x "
 z2 = '30+2*x'
 z3 = '40+2*x'
 plot(f1, f2, z1, z2, z3,(x, -15, 0), show=True, legend=True) 
 
 #3er cuadrante
-f1 = "8+x" # x1 + x2 <= 8
-f2 = "18+3*x" # 3x1 + x2 <= 18
-# Max Z = 2x1 + x2
-z1 = "20+2*x "
-z2 = '30+2*x'
-z3 = '40+2*x'
+#x1 <= 0, x2 <= 0 --> reescribimos la x1: -x1 >= 0 y la x2: -x2 >= 0
+f1 = "-8-x" # x1 + x2 <= 8
+f2 = "-18-3*x" # 3x1 + x2 <= 18
+
+z1 = "-20-2*x "
+z2 = '-30-2*x'
+z3 = '-40-2*x'
 plot(f1, f2, z1, z2, z3,(x, -15, 0), show=True, legend=True) 
 
 #4to cuadrante
 # x1 >= 0, x2 <= 0 --> reescribimos la x2: -x2 >= 0
-f1 = "-8+x" # x1 + x2 <= 8
-f2 = "-18+3*x" # 3x1 + x2 <= 18
-# Max Z = 2x1 + x2
+f1 = "-8+x" 
+f2 = "-18+3*x" 
+
 z1 = "-20+2*x "
 z2 = '-30+2*x'
 z3 = '-40+2*x'
